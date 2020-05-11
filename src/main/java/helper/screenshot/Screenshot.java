d88953fc6bdb532826f8cd11e2597e8f06830fe8
Rtyagi96 <62946931+Rtyagi96@users.noreply.github.com>
2020-05-11 14:11:31 +0200

    Update Screenshot.java

--- src/main/java/helper/screenshot/Screenshot.java
+++ src/main/java/helper/screenshot/Screenshot.java
@@ -15,22 +15,14 @@ public class Screenshot {
         private WebDriver driver;
 
         public static void takeSnapShot(WebDriver driver,String fileName) throws Exception{
-
-        //Convert web driver object to TakeScreenshot(Interface)..typecasting
-
+       
         TakesScreenshot scrShot =((TakesScreenshot)driver);
-
-        //Call getScreenshotAs method to create image file
 
         File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
 
         String timestamp = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
 
-        //Move image file to new destination
-
         File DestFile = new File(ResourceHelper.getResourcePath("/test-output/")+fileName+timestamp+".png");
-
-        //Copy file at destination
 
         FileUtils.copyFile(SrcFile,DestFile);
 
